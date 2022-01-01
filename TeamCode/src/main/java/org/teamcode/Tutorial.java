d8da0b29e702d221e2f98ce1df4ecbff6c1cecdf
Robert Kirk <67493652+11CaptainKirk@users.noreply.github.com>
2022-01-01 15:34:29 -0500

    changed op mode to reflect that this code is intended for teleop

--- TeamCode/src/main/java/org/teamcode/Tutorial.java
+++ TeamCode/src/main/java/org/teamcode/Tutorial.java
@@ -1,10 +1,10 @@
 package org.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="motor1")
+@TeleOp(name="test2")
 public class Tutorial extends OpMode {
 
     DcMotor motor;
