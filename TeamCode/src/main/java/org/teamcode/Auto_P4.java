5b2f4bcdcba3fd83e85c36e5e22e782d8b1847e3
Robert J Kirk <rk@robertjkirk.com>
2022-02-24 00:00:16 -0500

    modified autonomous names

--- TeamCode/src/main/java/org/teamcode/Auto_P4.java
+++ TeamCode/src/main/java/org/teamcode/Auto_P4.java
@@ -19,7 +19,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name = "Mecanum: Extravaganza Right", group = "Mecanum")
+@Autonomous(name = "Auto: P4", group = "Mecanum")
 public class AutonomousProgram extends LinearOpMode {
 
 
